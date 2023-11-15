e03d6b37ddc4c8dc3858c7aec32f507dfb5a3034
maddiepka <magdalena.m.plucinska@gmail.com>
2023-11-15 20:25:55 +0100

    render preparation 11

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY ='django-insecure-$qc*im%o7l+&qo7ehdn0=+7hn!-f!q631d5qa#5++r-r3v910g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "todoapp-tvod.onrender.com"]
 
