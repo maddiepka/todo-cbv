778089411891222ebe4a857a9d3c9b8981e9c419
maddiepka <magdalena.m.plucinska@gmail.com>
2023-11-15 14:55:32 +0100

    add requirements.txt

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import dj_database_url
+import os
 
 from pathlib import Path
 
@@ -81,7 +83,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+# DATABASE_URL = os.getenv('DATABASE_URL')
+# DATABASES = {
+#     'default': dj_database_url.config(),
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
