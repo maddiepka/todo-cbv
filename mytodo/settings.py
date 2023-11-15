fcfc8b02b3a1146fcebb0ef29ff3e799bd794849
maddiepka <magdalena.m.plucinska@gmail.com>
2023-11-15 21:13:48 +0100

    render preparation 12

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-insecure-$qc*im%o7l+&qo7ehdn0=+7hn!-f!q631d5qa#5++r-r3v910g'
+SECRET_KEY =os.environ.get('SECRET_KEY', 'django-insecure-$qc*im%o7l+&qo7ehdn0=+7hn!-f!q631d5qa#5++r-r3v910g')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == "True" 
